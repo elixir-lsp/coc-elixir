26a0cfe82af63f1e8100389fe398473c03124079
Amirali Esmaeili <esmailiamirali@gmail.com>
2019-06-27 19:48:49 +0430

    Add webpack build

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  target: 'node',
+  entry: './src/index.js',
+  externals: {
+    'coc.nvim': 'commonjs coc.nvim',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [{ loader: 'babel-loader' }],
+      },
+    ],
+  },
+  output: {
+    path: path.join(__dirname, 'lib'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs',
+  },
+};
